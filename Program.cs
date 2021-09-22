2362defc350e429d3fe10e3d38730d13ae97b412
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-09-22 20:58:45 +0300

    add types examples

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this is a comment
+            // int num1 = 4, // can assign multiple variables while initializing
+            //     num2 = 2,
+            //     num3 = 1;
+            double number = 1;
+            number = number + 1;
+            number += 3;
+            number -= 2;
+            number++;
+            number--;
+            number = 17;
+            number = number % 2; //modulo
+            number = 1.2;
+            int num1 = (int) number;
+            number = 0.2 + 0.2;
+            //char letter = 'a';
+            string word = "hello\n\n\n\t    \\ \aworld!";
+            word = @"hello\n\n\n\t    \\ \aworld!";
+            word = "hello" + " world!";
+            word += "!!!!!!!!!!";
+            bool foo = true; // foo, bar, baz
+            foo = false;
+            const double PI = 3.1415;
+            //int camelCase, StudlyCaps, snake_case; //different naming schemes
+            word = word.ToUpper();
+            Console.WriteLine(word);
         }
     }
 }
