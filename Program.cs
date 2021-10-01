7f8c227e6d5434527dd0c3be70619c8d08d0ff22
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-01 19:18:31 +0300

    insertion sort

--- Program.cs
+++ Program.cs
@@ -8,35 +8,28 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>();
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers.Add(10);
-            numbers[0] = 333;
-            Console.WriteLine(numbers[0]);
-            numbers.Insert(10, 444); // indeks peab olema addiga loodud
-            Console.WriteLine(numbers[10]);
-            bool contains = numbers.Contains(2);
-            Console.WriteLine(contains);
-
-            foreach(int num in numbers){
+            int[] array = {5,2,4,6,1,4,3};
+            int[] result = insertSort(array);
+            foreach(int num in result){
                 Console.WriteLine(num);
             }
-            var names = new List<string>()
-            {
-                "Kaspar",
-                "Martin",
-                "Suursalu"
-            };
-            Console.WriteLine(names.Count);
+        }
+
+        static int[] insertSort(int[] array) {
+            for(int i = 0; i<array.Length; i++){
+                int min = array[i];
+                int minPos = i;
+                for(int j = i; j<array.Length; j++){
+                    if(array[j] < min){
+                        min = array[j];
+                        minPos = j;
+                    }
+                }
+                int temp = array[i];
+                array[i] = min;
+                array[minPos] = temp;
+            }
+            return array;
         }
     }
 }
