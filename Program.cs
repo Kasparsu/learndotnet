6546e7a998f2445fd60f4973b76404e6a8ce1095
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-04 19:42:16 +0300

    add file write, append and read

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using System.IO;
 
 namespace learndotnet
 {
@@ -9,25 +7,12 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            DateTime time = new DateTime(1993, 03, 21, 14, 58, 33, 133);
-            Console.WriteLine(time);
-            Console.WriteLine(time.Date);
-            Console.WriteLine(time.Hour);
-            Console.WriteLine(time.Minute);
-            Console.WriteLine(time.Second);
-            Console.WriteLine(time.Millisecond);
-            Console.WriteLine(time.Day);
-            Console.WriteLine(time.Year);
-            Console.WriteLine(time.Month);
-            Console.WriteLine(time.DayOfWeek);
-            Console.WriteLine(time.DayOfYear);
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-            time = DateTime.Parse("2021-12-31");
-            Console.WriteLine(time);
-            time = time.AddDays(3);
-            Console.WriteLine(time);
-            Console.WriteLine(time.ToString("yyyy MMMM ddd"));
+            string text = "Hello files!asfasfsafa";
+            File.WriteAllText("file.txt", text);
+            text = "next line";
+            File.AppendAllText("file.txt", text);
+            text = File.ReadAllText("file.txt");
+            Console.Write(text);
         }
     }
 }
